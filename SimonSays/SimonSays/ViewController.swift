d976c4242792aa508e95ea65927571a62b5a851a
fete <d1swaay@gmail.com>
2016-10-20 22:56:04 -0400

    Adds a new Xcode project to the folder

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Fete Isfete on 10/20/16.
+//  Copyright © 2016 Fete Isfete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
