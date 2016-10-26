84e18c745508609804b99e3a4a504bb6ede300ad
edmund-h <447ght@gmail.com>
2016-10-26 12:44:51 -0400

    this is whetting my appetite to build an actual game

---
+++ ios Interface Builder Lab/ios Interface Builder LabTests/ios_Interface_Builder_LabTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_Interface_Builder_LabTests.swift
+//  ios Interface Builder LabTests
+//
+//  Created by Edmund Holderbaum on 10/26/16.
+//  Copyright © 2016 Edmund Holderbaum. All rights reserved.
+//
+
+import XCTest
+@testable import ios_Interface_Builder_Lab
+
+class ios_Interface_Builder_LabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
