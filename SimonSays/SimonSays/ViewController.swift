45e177d738f13490c6d7c57f886b451d0ccf986b
aviflombaum <avi@flombaum.com>
2016-11-18 20:10:05 -0500

    Simon says

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Avi Flombaum on 11/18/16.
+//  Copyright © 2016 Avi Flombaum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
