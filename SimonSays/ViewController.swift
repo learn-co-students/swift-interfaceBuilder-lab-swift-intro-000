96e1d08e2bcc49600470ea8af57af904046d7a6e
Jeffrey Eugene Hoch <jeffreyeugenehoch@gmail.com>
2017-01-09 04:59:28 -0500

    lab completed

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Jeffrey Eugene Hoch on 1/9/17.
+//  Copyright © 2017 Jeffrey Eugene Hoch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
