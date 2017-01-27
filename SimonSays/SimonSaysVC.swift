15c5ef610aa827b88494d96a28462ac397ae3e73
Etty427 <etty427@users.noreply.github.com>
2017-01-26 21:59:12 -0500

    added interface

---
+++ SimonSays/SimonSaysVC.swift
@@ -0,0 +1,35 @@
+//
+//  SimonSaysVC.swift
+//  SimonSays
+//
+//  Created by Etnuh on 1/26/17.
+//  Copyright © 2017 Rainman Technologies. All rights reserved.
+//
+
+import UIKit
+
+class SimonSaysVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
