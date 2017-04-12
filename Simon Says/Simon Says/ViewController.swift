9851ddbc6f745a6e2707ac34c4e2091fca22aa84
Brian Harrington <brianharrington715@gmail.com>
2017-04-11 21:42:43 -0700

    completed lab

---
+++ Simon Says/Simon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Brian Harrington on 4/11/17.
+//  Copyright © 2017 sevenonefive.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
