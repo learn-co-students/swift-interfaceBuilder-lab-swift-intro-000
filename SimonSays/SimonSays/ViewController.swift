37cc2866ab9b09465582548ac3764aa2c484dbae
jcornelussen <j.cornelussen@planet.nl>
2017-04-23 18:22:45 +0200

    Simon Says interface build

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by John Cornelussen on 23-04-17.
+//  Copyright © 2017 John Cornelussen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
