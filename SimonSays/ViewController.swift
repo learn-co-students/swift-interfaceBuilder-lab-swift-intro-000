9f657a13a2fc20bf16761dee8924f6f742024eb2
Jing Lin Ruan <jinglinruan@gmail.com>
2017-05-26 00:45:54 -0400

    Interface Builder

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Jinglin Ruan on 2017-05-26.
+//  Copyright © 2017 Jinglin Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
