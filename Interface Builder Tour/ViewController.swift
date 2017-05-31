e393f28fe16f23c2cbb88e36e936cc9c7fb9ab55
incognito2170 <incognito2170@gmail.com>
2017-05-31 11:41:49 +0600

    Finished Interface Builder Lab 1

---
+++ Interface Builder Tour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interface Builder Tour
+//
+//  Created by Arabi Hasan on 5/29/17.
+//  Copyright © 2017 Arabi Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
