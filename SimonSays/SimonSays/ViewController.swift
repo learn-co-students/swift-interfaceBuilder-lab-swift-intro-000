0e1a436a2d5730fd11b218f7b5bc1101b826a89e
Whoopinstick <k.m.paul68@gmail.com>
2017-07-05 22:22:31 -0400

    complete lab

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Kevin Paul on 7/5/17.
+//  Copyright © 2017 Whoopinstick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
