c6e36a80cad6be2559a91173ff69a28704eafe62
tormic <tormic1992@gmail.com>
2017-07-30 14:25:51 +0200

    done

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Tor Magnus Michaelsen on 30/07/2017.
+//  Copyright © 2017 Tor Magnus Michaelsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
