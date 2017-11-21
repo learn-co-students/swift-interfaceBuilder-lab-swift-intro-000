3e346b622e2e494395820ffa07faff9f48e0fea7
Dane Wallace <danewallace@icloud.com>
2017-11-21 00:42:20 -0500

    Completed

---
+++ Interface Builder Lab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Interface Builder Lab
+//
+//  Created by Dane Wallace on 11/21/17.
+//  Copyright © 2017 Dane Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
