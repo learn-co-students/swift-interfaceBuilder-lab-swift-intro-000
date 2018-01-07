c923ed0119c30ac297335b5bfe6eabbe107e9569
allysonw <allysonwesman@gmail.com>
2018-01-06 20:20:54 -0800

    Created new project

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Allyson Wesman on 1/6/18.
+//  Copyright © 2018 Allyson Wesman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
