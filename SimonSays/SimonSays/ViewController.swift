9290189c48c406d929123ad3d24d6ef844e9297d
Hashiba <training@hashiba.org>
2018-01-19 21:23:42 -0300

    My version of the SimonSays UI

---
+++ SimonSays/SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by Hashiba on 1/19/18.
+//  Copyright © 2018 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
