fce582aadcd71ec42f9338475379c3f012b7b75b
vinodkalpka <vinod.wst@gmail.com>
2018-03-08 14:45:55 +0530

    done
    done

---
+++ swift-interfaceBuilder-lab-swift-intro-000/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-interfaceBuilder-lab-swift-intro-000
+//
+//  Created by Sreekumar J  on 08/03/18.
+//  Copyright © 2018 Webnetwork Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
