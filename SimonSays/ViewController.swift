0317538bf35c3cc8501621f3696b5069756ef079
chelcastel <ccastelli12@aol.com>
2018-04-11 18:03:58 -0400

    simon says

---
+++ SimonSays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimonSays
+//
+//  Created by ChelseaAnne Castelli on 4/11/18.
+//  Copyright © 2018 ChelseaAnne Castelli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
