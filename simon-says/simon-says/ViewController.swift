cfd40228331744112d6ae2df3f2476d4442a700a
Anthony Keating <anthonyk1225@gmail.com>
2019-01-11 12:30:50 -0500

    complete

---
+++ simon-says/simon-says/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simon-says
+//
+//  Created by Code on 1/10/19.
+//  Copyright © 2019 Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
